ab7fa8fb034eefb79c3f2f9c9766866883b9df5f
Severin Kistler <kistlers@ethz.ch>
2021-07-30 10:58:41 +0200

    very smple non-pspin send motif

---
+++ src/sst/elements/ember/mpi/motifs/embersend.cc
@@ -0,0 +1,95 @@
+// Copyright 2009-2021 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2021, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#include <sst_config.h>
+
+// sst_config must be at top
+
+#include "embersend.h"
+
+using namespace SST::Ember;
+
+#define TAG 0xDEADBEEF
+
+EmberSendGenerator::EmberSendGenerator(SST::ComponentId_t id, Params &params)
+    : EmberMessagePassingGenerator(id, params, "Send"),
+      m_loopIndex(0),
+      m_rank2(1) {
+    m_elementCount = (uint32_t)params.find("arg.elementCount", 112);
+    m_iterations = (uint32_t)params.find("arg.iterations", 1);
+    m_rank2 = (uint32_t)params.find("arg.rank2", 1);
+    m_verify = params.find<bool>("arg.verify", true);
+
+    memSetBackedZeroed();
+    m_messageSize = m_elementCount * sizeofDataType(INT);
+    m_sendBuf = memAlloc(m_messageSize);
+    m_recvBuf = memAlloc(m_messageSize);
+
+    int32_t *sendBufElements = (int32_t *)m_sendBuf;
+    for (int i = 0; i < m_elementCount; i++) {
+        sendBufElements[i] = 100 * rank() + i;
+    }
+}
+
+bool EmberSendGenerator::generate(std::queue<EmberEvent *> &evQ) {
+    if (m_loopIndex == m_iterations || !(0 == rank() || m_rank2 == rank())) {
+        if (0 == rank()) {
+            double totalTime = (double)(m_stopTime - m_startTime) / 1000000000.0;
+
+            double latency = ((totalTime / m_iterations) / 2);
+            double bandwidth = (double)m_messageSize / latency;
+
+            output(
+                "%s: otherRank %d, total time %.3f us, loop %d, bufLen %d"
+                ", latency %.3f us. bandwidth %f GB/s\n",
+                getMotifName().c_str(), m_rank2, totalTime * 1000000.0, m_iterations, m_messageSize,
+                latency * 1000000.0, bandwidth / 1000000000.0);
+        }
+
+        if (m_rank2 == rank() && m_verify) {
+            int32_t *recvBufElements = (int32_t *)m_recvBuf;
+            for (int i = 0; i < m_elementCount; i++) {
+                int32_t shouldEqual = 100 * otherRank() + i + 1;
+                if (shouldEqual != recvBufElements[i]) {
+                    printf("Error: Rank %d index %d failed  got=%d shouldEqual=%d\n", rank(), i, recvBufElements[i],
+                           shouldEqual);
+                }
+            }
+        }
+        return true;
+    }
+
+    if (0 == m_loopIndex) {
+        verbose(CALL_INFO, 1, 0, "rank=%d size=%d\n", rank(), size());
+
+        if (0 == rank()) {
+            output("rank2=%d messageSize=%d iterations=%d\n", m_rank2, m_messageSize, m_iterations);
+            enQ_getTime(evQ, &m_startTime);
+        }
+    }
+
+    if (0 == rank()) {
+        enQ_send(evQ, m_sendBuf, m_messageSize, CHAR, m_rank2, TAG, GroupWorld);
+        // enQ_recv( evQ, m_recvBuf, m_messageSize, CHAR, m_rank2, TAG, GroupWorld, &m_resp );
+    } else if (m_rank2 == rank()) {
+        enQ_recv(evQ, m_recvBuf, m_messageSize, CHAR, 0, TAG, GroupWorld, &m_resp);
+        // enQ_send( evQ, m_sendBuf, m_messageSize, CHAR, 0, TAG, GroupWorld );
+    }
+
+    if (++m_loopIndex == m_iterations) {
+        enQ_getTime(evQ, &m_stopTime);
+    }
+    return false;
+}
