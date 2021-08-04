8f57fade38ca9918f7b41b15b18655c8aad24031
Severin Kistler <kistlers@ethz.ch>
2021-08-04 12:16:08 +0200

    update pspinSend

--- src/sst/elements/ember/mpi/motifs/emberpspinsend.cc
+++ src/sst/elements/ember/mpi/motifs/emberpspinsend.cc
@@ -30,18 +30,16 @@ EmberPspinSendGenerator::EmberPspinSendGenerator(SST::ComponentId_t id, Params &
     m_rank2 = (uint32_t)params.find("arg.rank2", 1);
     m_verify = params.find<bool>("arg.verify", true);
 
-    memSetBacked();
+    memSetBackedZeroed();
     m_messageSize = ROUND_UP_DMA_WIDTH(sizeof(pspin_pkt_header_t)) + m_count * sizeofDataType(INT);
     m_sendBuf = memAlloc(m_messageSize);
     m_recvBuf = memAlloc(m_messageSize);
-    memset(m_sendBuf, 0, m_messageSize);
-    memset(m_recvBuf, 0, m_messageSize);
 
     pspin_pkt_header_t *header = (pspin_pkt_header_t *)m_sendBuf;
     header->source = rank();
     header->destination = otherRank();
     output("rank %u: header->source=%u header->destination=%d\n", rank(), header->source, header->destination);
-    
+
     int32_t *sendBufElements = (int32_t *)((char *)m_sendBuf + ROUND_UP_DMA_WIDTH(sizeof(pspin_pkt_header_t)));
     for (int i = 0; i < m_count; i++) {
         sendBufElements[i] = 100 * rank() + i;
@@ -65,7 +63,20 @@ bool EmberPspinSendGenerator::generate(std::queue<EmberEvent *> &evQ) {
 
         if (m_rank2 == rank() && m_verify) {
             std::function<uint64_t()> verify = [&]() {
-                int32_t *recvBufElements = (int32_t *)m_recvBuf;
+                pspin_pkt_header_t *header = (pspin_pkt_header_t *)m_sendBuf;
+
+                if (header->source != otherRank()) {
+                    printf("Error: Rank %d header->source failed  got=%d shouldEqual=%d\n", rank(), header->source,
+                           otherRank());
+                }
+                if (header->destination != rank()) {
+                    printf("Error: Rank %d header->source failed  got=%d shouldEqual=%d\n", rank(), header->destination,
+                           rank());
+                }
+
+                int32_t *recvBufElements =
+                    (int32_t *)((char *)m_recvBuf + ROUND_UP_DMA_WIDTH(sizeof(pspin_pkt_header_t)));
+                ;
                 for (int i = 0; i < m_count; i++) {
                     int32_t shouldEqual = 100 * otherRank() + i;
                     if (shouldEqual != recvBufElements[i]) {
--- src/sst/elements/ember/mpi/motifs/emberpspinsend.h
+++ src/sst/elements/ember/mpi/motifs/emberpspinsend.h
@@ -13,7 +13,6 @@
 // information, see the LICENSE file in the top level directory of the
 // distribution.
 
-
 #ifndef _H_EMBER_PSPIN_SEND
 #define _H_EMBER_PSPIN_SEND
 
@@ -24,72 +23,55 @@ namespace SST {
 namespace Ember {
 
 class EmberPspinSendGenerator : public EmberMessagePassingGenerator {
+   public:
+    SST_ELI_REGISTER_SUBCOMPONENT_DERIVED(EmberPspinSendGenerator, "ember", "PspinSendMotif",
+                                          SST_ELI_ELEMENT_VERSION(1, 0, 0), "Performs a Pspin Send Motif",
+                                          SST::Ember::EmberGenerator)
 
-public:
-    SST_ELI_REGISTER_SUBCOMPONENT_DERIVED(
-        EmberPspinSendGenerator,
-        "ember",
-        "PspinSendMotif",
-        SST_ELI_ELEMENT_VERSION(1,0,0),
-        "Performs a Pspin Send Motif",
-        SST::Ember::EmberGenerator
-    )
-
-    SST_ELI_DOCUMENT_PARAMS(
-        {   "arg.count",      "Sets the number of elements in the send operation", "112"},
-        {   "arg.iterations",       "Sets the number of send operations to perform",   "1"},
-        {   "arg.rank2",        "Sets the 2nd rank to send to (0 is the 1st)",    "1"},
-    )
+    SST_ELI_DOCUMENT_PARAMS({"arg.count", "Sets the number of elements in the send operation", "112"},
+                            {"arg.iterations", "Sets the number of send operations to perform", "1"},
+                            {"arg.rank2", "Sets the 2nd rank to send to (0 is the 1st)", "1"}, )
     SST_ELI_DOCUMENT_STATISTICS(
-        { "time-Init", "Time spent in Init event",          "ns",  0},
-        { "time-Finalize", "Time spent in Finalize event",  "ns", 0},
-        { "time-Rank", "Time spent in Rank event",          "ns", 0},
-        { "time-Size", "Time spent in Size event",          "ns", 0},
-        { "time-Send", "Time spent in Recv event",          "ns", 0},
-        { "time-Recv", "Time spent in Recv event",          "ns", 0},
-        { "time-Irecv", "Time spent in Irecv event",        "ns", 0},
-        { "time-Isend", "Time spent in Isend event",        "ns", 0},
-        { "time-Wait", "Time spent in Wait event",          "ns", 0},
-        { "time-Waitall", "Time spent in Waitall event",    "ns", 0},
-        { "time-Waitany", "Time spent in Waitany event",    "ns", 0},
-        { "time-Compute", "Time spent in Compute event",    "ns", 0},
-        { "time-Barrier", "Time spent in Barrier event",    "ns", 0},
-        { "time-Alltoallv", "Time spent in Alltoallv event", "ns", 0},
-        { "time-Alltoall", "Time spent in Alltoall event",  "ns", 0},
-        { "time-Allreduce", "Time spent in Allreduce event", "ns", 0},
-        { "time-Reduce", "Time spent in Reduce event",      "ns", 0},
-        { "time-Bcast", "Time spent in Bcast event",        "ns", 0},
-        { "time-Gettime", "Time spent in Gettime event",    "ns", 0},
-        { "time-Commsplit", "Time spent in Commsplit event", "ns", 0},
-        { "time-Commcreate", "Time spent in Commcreate event", "ns", 0},
-    )
+        {"time-Init", "Time spent in Init event", "ns", 0}, {"time-Finalize", "Time spent in Finalize event", "ns", 0},
+        {"time-Rank", "Time spent in Rank event", "ns", 0}, {"time-Size", "Time spent in Size event", "ns", 0},
+        {"time-Send", "Time spent in Recv event", "ns", 0}, {"time-Recv", "Time spent in Recv event", "ns", 0},
+        {"time-Irecv", "Time spent in Irecv event", "ns", 0}, {"time-Isend", "Time spent in Isend event", "ns", 0},
+        {"time-Wait", "Time spent in Wait event", "ns", 0}, {"time-Waitall", "Time spent in Waitall event", "ns", 0},
+        {"time-Waitany", "Time spent in Waitany event", "ns", 0},
+        {"time-Compute", "Time spent in Compute event", "ns", 0},
+        {"time-Barrier", "Time spent in Barrier event", "ns", 0},
+        {"time-Alltoallv", "Time spent in Alltoallv event", "ns", 0},
+        {"time-Alltoall", "Time spent in Alltoall event", "ns", 0},
+        {"time-Allreduce", "Time spent in Allreduce event", "ns", 0},
+        {"time-Reduce", "Time spent in Reduce event", "ns", 0}, {"time-Bcast", "Time spent in Bcast event", "ns", 0},
+        {"time-Gettime", "Time spent in Gettime event", "ns", 0},
+        {"time-Commsplit", "Time spent in Commsplit event", "ns", 0},
+        {"time-Commcreate", "Time spent in Commcreate event", "ns", 0}, )
 
-public:
-	EmberPspinSendGenerator(SST::ComponentId_t, Params& params);
-    bool generate( std::queue<EmberEvent*>& evQ);
+   public:
+    EmberPspinSendGenerator(SST::ComponentId_t, Params& params);
+    bool generate(std::queue<EmberEvent*>& evQ);
 
-    uint32_t otherRank() {
-        return rank() == m_rank2 ? 0 : m_rank2;
-    }
+    uint32_t otherRank() { return rank() == m_rank2 ? 0 : m_rank2; }
 
-private:
-    MessageRequest  m_req;
+   private:
+    MessageRequest m_req;
     MessageResponse m_resp;
-    void*    m_sendBuf;
-    void*    m_recvBuf;
+    void* m_sendBuf;
+    void* m_recvBuf;
 
-	uint32_t m_count;
-	uint32_t m_messageSize;
-	uint32_t m_iterations;
+    uint32_t m_count;
+    uint32_t m_messageSize;
+    uint32_t m_iterations;
     uint64_t m_startTime;
     uint64_t m_stopTime;
     uint32_t m_loopIndex;
 
-    int      m_rank2;
-    bool     m_verify;
+    int m_rank2;
+    bool m_verify;
 };
 
-}
-}
+}  // namespace Ember
+}  // namespace SST
 
 #endif
